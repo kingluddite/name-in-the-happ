e992dc63db0bfb00f27c5791da7c2a3e3ab20338
PEH2 <howley.phil@gmail.com>
2017-05-21 23:55:09 -0700

    Setup state for title/body. Add handleDeletePresentation method

--- imports/ui/components/presentations/EditPresentation.js
+++ imports/ui/components/presentations/EditPresentation.js
@@ -3,23 +3,54 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Session } from 'meteor/session';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
+import { browserHistory } from 'react-router';
 
 // collections
 import PresentationsCollection from './../../../api/presentations';
 
 export class EditPresentation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      body: '',
+    };
+  }
+
+  // componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
+    const currentPresentationId = this.props.presentation ? this.props.presentation._id : undefined;
+    const prevPresentationId = prevProps.presentation ? prevProps.presentation._id : undefined;
+
+    if (currentPresentationId && currentPresentationId !== prevPresentationId) {
+      this.setState({
+        title: this.props.presentation.title,
+        body: this.props.presentation.body,
+      });
+    }
+  }
+
   handleTitleChange(event) {
+    const title = event.target.value;
+    this.setState({ title });
     this.props.call('presentations.update', this.props.presentation._id, {
-      title: event.target.value,
+      title,
     });
   }
 
   handleBodyChange(event) {
+    const body = event.target.value;
+    this.setState({ body });
     this.props.call('presentations.update', this.props.presentation._id, {
-      body: event.target.value,
+      body,
     });
   }
 
+  handleDeletePresentation() {
+    this.props.call('presentations.remove', this.props.presentation._id);
+    this.props.browserHistory.push('/presentations');
+  }
 
   render() {
     if (this.props.presentation) {
@@ -27,13 +58,17 @@ export class EditPresentation extends Component {
         <div>
          <input
            type="text"
-           value={this.props.presentation.title}
+           value={this.state.title}
            placeholder="title"
            onChange={this.handleTitleChange.bind(this)} />
          <textarea
-           value={this.props.presentation.body}
+           value={this.state.body}
            placeholder="Your presentation here" onChange={this.handleBodyChange.bind(this)} />
-         <button className="button">Delete Presentation</button>
+         <button
+           className="button"
+           onClick={this.handleDeletePresentation.bind(this)}>
+           Delete Presentation
+         </button>
        </div>
       );
     }
@@ -49,6 +84,7 @@ EditPresentation.propTypes = {
   selectedPresentationId: PropTypes.string,
   presentation: PropTypes.object,
   call: PropTypes.func.isRequired,
+  browserHistory: PropTypes.object.isRequired,
 };
 
 export default createContainer(() => {
@@ -58,5 +94,6 @@ export default createContainer(() => {
     selectedPresentationId,
     presentation: PresentationsCollection.findOne(selectedPresentationId),
     call: Meteor.call,
+    browserHistory,
   };
 }, EditPresentation);
