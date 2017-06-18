65800ddf79b57b1e915498dc5f03954cc2db6e98
PEH2 <howley.phil@gmail.com>
2017-06-18 00:21:07 -0700

    style play presentation page

--- imports/ui/components/presentations/PlayPresentation.js
+++ imports/ui/components/presentations/PlayPresentation.js
@@ -4,13 +4,23 @@ import PropTypes from 'prop-types';
 export const PlayPresentation = (props) => {
   return (
     <div>
-       Number Left To Present {props.remainingPresenters}
-       Next Presenter: {props.onDeckName}
-      <div>
-        <span>Presenter</span>
-        <h2 className="watch__heading">
-          Name: {props.currentPresenterName}
-        </h2>
+      <div className="play-presentation">
+        <div className="play-presentation__current-presenters">
+          <h2 className="play-presentation__title">Now Presenting</h2>
+          <span className="play-presentation__current-presenters-count">
+            {props.currentPresenterName}</span>
+        </div>
+      </div>
+      <div className="play-presentation">
+        <div className="play-presentation__remaining-presenters">
+          <h2 className="play-presentation__subtitle">Remaining</h2>
+          <span className="play-presentation__presenters-count">{props.remainingPresenters}</span>
+        </div>
+        <div className="play-presentation__remaining-presenters">
+          <h2 className="play-presentation__subtitle">On Deck</h2>
+          <span className="play-presentation__presenters-count">
+            {props.onDeckName}</span>
+        </div>
       </div>
       <div>
         <button className="button" onClick={props.nextPresenter}>Next</button>
